9935fc24be82edad89dcde6e1733541808ce2746
veronicamagnanimi <veronica.magnanimi@hotmail.it>
2024-11-04 15:37:59 +0100

    feat: dadi

---
+++ DADI/SCRIPT/script.js
@@ -0,0 +1,19 @@
+// NUMERI RANDOM
+const firstNum = Math.floor(Math.random() * 6) + 1;
+const secondNum = Math.floor(Math.random() * 6) + 1;
+console.log(`Il punteggio del primo gicoatore è ${firstNum}`);
+console.log(`Il punteggio del secondo giocatore è ${secondNum}`);
+
+// ESECUZIONE LOGICA
+if (firstNum > secondNum) {
+    console.log("Vince il primo giocatore");
+} else if (firstNum < secondNum) {
+    console.log("Vince il secondo giocatore");
+} else {
+    console.log("Pareggio!");
+}
+
+
+
+
+
---
+++ MAIL/README.md
@@ -0,0 +1,10 @@
+DATI
+- Lista delle email
+- Email utente
+
+ESECUZIONE LOGICA 
+- Chiedo all'utente la sua email
+- Data una lista di email (potrebbe essere un array) utilizzo il ciclo per verificare se all'interno è presente, se la trova esco dal ciclo
+
+OUTPUT
+- Se è presente in console avrò un messaggio di accesso ok, altrimenti avrò un accesso negato 
